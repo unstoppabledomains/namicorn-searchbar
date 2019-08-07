a450820239718dc0eeea03fd96c4b7f44e7bdaab
Johnny <johnny@unstoppabledomains.com>
2019-08-07 08:32:43 -0700

    Updates

---
+++ precache-manifest.482433833f0e0c8439d5d558320456d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e9afecf205b70a4f8d81161eee252f",
+    "url": "/namicorn-searchbar/index.html"
+  },
+  {
+    "revision": "6e5bb11373e64628c43c",
+    "url": "/namicorn-searchbar/static/css/main.a939bb18.chunk.css"
+  },
+  {
+    "revision": "6b58f68e20aeb51e1a24",
+    "url": "/namicorn-searchbar/static/js/2.1647d9a7.chunk.js"
+  },
+  {
+    "revision": "6e5bb11373e64628c43c",
+    "url": "/namicorn-searchbar/static/js/main.720bc946.chunk.js"
+  },
+  {
+    "revision": "e8bafcd0f3191c310a2c",
+    "url": "/namicorn-searchbar/static/js/runtime~main.e71deda6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d8d3de6a6d58153bb518ab990102471.js"
+  "/namicorn-searchbar/precache-manifest.482433833f0e0c8439d5d558320456d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/namicorn-searchbar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
